37e809b778bd6263e4ee2f93f557bea9a8ded5d9
Zak Morgan <zaksmorgan@protonmail.com>
2024-08-26 14:09:56 +0100

    Add flircamera mock in-case spinnaker sdk not installed

---
+++ flircamera_mock.py
@@ -0,0 +1,23 @@
+
+
+class CameraManager():
+    def __init__(self):
+        pass
+
+    def get_camera(self):
+        pass
+
+    def setup_camera(self):
+        pass
+
+    def begin_acquisition(self):
+        pass
+
+    def end_acquisition(self):
+        pass
+
+    def capture_frame(self):
+        pass
+
+    def release_camera(self, acquisition_status):
+        pass
